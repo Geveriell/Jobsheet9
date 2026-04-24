522a7fe6c8719711321efe8b05467f0c0c170a99
Geveriell <erzaardhyaputra@gmail.com>
2026-04-24 11:57:17 +0700

    2.2.1

---
+++ jobsheet9/StackTugasMahasiswa07.java
@@ -0,0 +1,85 @@
+package jobsheet9;
+
+public class StackTugasMahasiswa07 {
+    Mahasiswa07[] stack;
+    int size;
+    int top;
+    public StackTugasMahasiswa07(int size) {
+        this.size = size;
+        stack = new Mahasiswa07[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa07 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi.");
+        }
+    }
+
+    public Mahasiswa07 pop() {
+        if (!isEmpty()) {
+            Mahasiswa07 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas untuk dinilai.");
+            return null;
+        }
+    }
+
+    public Mahasiswa07 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan.");
+            return null;
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(
+                    stack[i].nama + "\t" +
+                    stack[i].nim + "\t" +
+                    stack[i].kelas
+            );
+        }
+        System.out.println("");
+    }
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi07 stack = new StackKonversi07();
+
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+
+        String biner = "";
+
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+
+        return biner;
+    }
+}
