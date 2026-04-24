fc0d7ce853fd2e50964acbfbad05abb517434895
Geveriell <erzaardhyaputra@gmail.com>
2026-04-24 11:57:35 +0700

    2.2.1

---
+++ jobsheet9/Mahasiswa07.java
@@ -0,0 +1,23 @@
+package jobsheet9;
+
+public class Mahasiswa07 {
+    String nama;
+    String nim;
+    String kelas;
+    int nilai;
+
+    public Mahasiswa07() {
+
+    }
+
+    public Mahasiswa07(String nama, String nim, String kelas) {
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+    }
+
+    void tugasDinilai(int nilai) {
+        this.nilai = nilai;
+    }
+}
